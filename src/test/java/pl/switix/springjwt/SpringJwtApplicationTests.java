25f4f1908b991ef7922b535725385bf5b01ea6cc
Maciej Świtalski <macie789@wp.pl>
2021-10-02 13:07:22 +0200

    git init

---
+++ src/test/java/pl/switix/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.switix.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
